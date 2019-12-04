a58fde69146a3c0b3efefa2afdf94d775f93740a
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2019-12-04 18:58:15 -0300

    Add files via upload

--- concepcao-estruturada-de-circuitos-integrados/ADDAC-antes/acc/goldenmodel/acc.c
+++ concepcao-estruturada-de-circuitos-integrados/ADDAC-antes/acc/goldenmodel/acc.c
@@ -5,18 +5,17 @@ void acc(int clka, int clk, int a, int acu){
     int saida;
     FILE *fp;
     fp = fopen("tabela-acc.tv", "w");
-    fprintf(fp, "clock-anterior;clock;entrada;acumulado;saida\n");
-    for(clka = 0; clka < 2; clka++){
+    fprintf(fp, "clock;entrada;acumulado;saida\n");
         for (clk = 0; clk < 2; clk++) {
             for(a = 0; a < 2; a++){
                 for( int acu = 0; acu < 2; acu++){
-                    if(clka == 1 && clk == 0) saida = a;
+                    if(clk == 0) saida = a;
                     else saida = acu;
-                    fprintf(fp, "%d%d%d%d_%d\n", clka, clk, a, acu, saida);
+                    fprintf(fp, "%d_%d_%d_%d\n",clk, a, acu, saida);
                 }
             }
         }
-    }
+    
     fclose(fp);
 }
 
---
+++ concepcao-estruturada-de-circuitos-integrados/ADDAC/addac/goldenmodel/addac.c
@@ -0,0 +1,75 @@
+#include "../acc/goldenmodel/acc.h"
+#include "../inv/goldenmodel/inv.h"
+#include "../somador/goldenmodel/somador.h"
+#include "../mux/goldenmodel/mux.h"
+int main(){
+    int sel0, sel1, clk = 0, clka = 1, a = 0, mux0 = 0, mux1 = 0, soma = 0, c_out = 0, s, ac = 0;
+    
+    FILE *fp;
+    fp = fopen("addac-tb.tv", "w");
+    fprintf(fp, "sel0_sel1_a_clk_acu_saida\n");
+    sel0 = 0;
+    sel1 = 0;
+    for(a = 0; a < 2; a++){
+        for(ac = 0; ac < 2; ac++){
+            for(clk = 0; clk < 2; clk++){
+                mux0 = mux(a, inv(a), sel0);
+                somador(mux0, ac, sel0, &soma, &c_out);
+                mux1 = mux(mux0, soma, sel1);
+                ac = acc(clka, clk, mux1, ac);
+                fprintf(fp, "%d_%d_%d_%d_%d_%d_%d\n", sel0, sel1, a, ac, clk, mux1, ac);
+                
+            }
+            clka = clk;
+        }
+        
+    }
+    sel0 = 0;
+    sel1 = 1;
+    for(a = 0; a < 2; a++){
+        for(ac = 0; ac < 2; ac++){
+            for(clk = 0; clk < 2; clk++){
+                mux0 = mux(a, inv(a), sel0);
+                somador(mux0, ac, sel0, &soma, &c_out);
+                mux1 = mux(mux0, soma, sel1);
+                ac = acc(clka, clk, mux1, ac);
+                fprintf(fp, "%d_%d_%d_%d_%d_%d_%d\n", sel0, sel1, a, ac, clk, mux1, ac);
+                
+            }
+            clka = clk;
+        }
+        
+    }
+    sel0 = 1;
+    sel1 = 0;
+    for(a = 0; a < 2; a++){
+        for(ac = 0; ac < 2; ac++){
+            for(clk = 0; clk < 2; clk++){
+                mux0 = mux(a, inv(a), sel0);
+                somador(mux0, ac, sel0, &soma, &c_out);
+                mux1 = mux(mux0, soma, sel1);
+                ac = acc(clka, clk, mux1, ac);
+                fprintf(fp, "%d_%d_%d_%d_%d_%d_%d\n", sel0, sel1, a, ac, clk, mux1, ac);
+                
+            }
+            clka = clk;
+        }
+    }
+    sel0 = 1;
+    sel1 = 1;
+    for(a = 0; a < 2; a++){
+        for(ac = 0; ac < 2; ac++){
+            for(clk = 0; clk < 2; clk++){
+                mux0 = mux(a, inv(a), sel0);
+                somador(mux0, ac, sel0, &soma, &c_out);
+                mux1 = mux(mux0, soma, sel1);
+                ac = acc(clka, clk, mux1, ac);
+                fprintf(fp, "%d_%d_%d_%d_%d_%d_%d\n", sel0, sel1, a, ac, clk, mux1, ac);
+                
+            }
+            clka = clk;
+        }
+    }
+    fclose(fp);
+    return 0;
+}
