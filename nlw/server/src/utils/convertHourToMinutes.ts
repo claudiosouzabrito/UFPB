68c82a91738123b0bc26cfb0dc917a479cdbbf47
claudiosouzabrito <claudiosouzabrito@gmail.com>
2022-03-21 07:39:20 -0300

    .

--- nlw/server/src/utils/convertHourToMinutes.ts
+++
@@ -1,5 +0,0 @@
-export default function convertHourToMinutes(time: string){
-    const [hour, minutes] = time.split(':').map(Number);
-    const timeInMinutes = (hour * 60) + minutes;
-    return timeInMinutes
-}
